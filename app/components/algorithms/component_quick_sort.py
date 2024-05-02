17b85b713d4bf3493258b19dcc26774ffc834a49
Felipe Oliveira <flipeaz342@gmail.com>
2024-05-01 23:39:42 -0400

    organize some components

--- app/components/algorithms/component_quick_sort.py
+++ app/components/algorithms/component_quick_sort.py
@@ -1,5 +1,190 @@
 import dash_bootstrap_components as dbc
+from dash import html
+
+
 
 layout = dbc.Col([
+    dbc.Row([
+        dbc.Col([
+            dbc.CardGroup([
+                dbc.Card([
+                html.H2('Quick Sort', className='card-title', style={'text-align': 'center'}),
+            ])
+            ])
+        ])
+    ], style={'padding-top': '20px'}),
 
-], md=10)
+
+    
+    dbc.Row([
+        # Lista com 10 elementos
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('10 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+
+        # Lista com 50 elementos
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('50 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+
+        # Lista com 100 elementos
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('100 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+    ], style={'padding-top': '20px'}),
+
+
+    dbc.Row([
+
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('200 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+
+        
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('500 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+
+        
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('800 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+    ], style={'padding-top': '20px'}),
+
+
+    dbc.Row([
+        # Lista com 10 elementos
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('1000 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+
+
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('1500 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+
+        
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('2000 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+    ], style={'padding-top': '20px'}),
+
+
+
+     dbc.Row([
+        # Lista com 10 elementos
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('3500 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+
+
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('4500 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+
+        
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('5500 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+    ], style={'padding-top': '20px'}),
+
+
+    dbc.Row([
+        # Lista com 10 elementos
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('6500 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+
+
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('7500 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+
+        
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('8500 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+    ], style={'padding-top': '20px'}),
+
+
+    dbc.Row([
+        # Lista com 10 elementos
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('9500 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+
+
+        dbc.Col([
+            dbc.CardGroup([
+            dbc.Card([
+                html.Legend('10.000 Elementos')
+            ], style={'padding-left': '16px', 'padding-top': '10px'}),
+        ])
+        ], width=4),
+    ], style={'padding-top': '20px'}),
+    
+], style={'margin': '10px'})
--- app/components/sidebar.py
+++ app/components/sidebar.py
@@ -1,12 +1,11 @@
 
 import os
+import random
 import dash
 from dash import html, dcc
 from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
-from app import app
-
-import time
+from app import app 
 
 from datetime import datetime as dt, date
 import plotly.express as px
@@ -36,8 +35,8 @@ layout = dbc.Col([
     ]),
 
 
-    dbc.Alert('Arays deletados com sucesso!', color='warning', id='alert-delete', is_open=False),
-    dbc.Alert('Arays gerados com sucesso!', color='success', id='alert-generate', is_open=False),
+    dbc.Alert('Arrays gerados com sucesso!', color='success', id='alert-generate', is_open=False),
+    dbc.Alert('Arrays deletados com sucesso!', color='warning', id='alert-delete', is_open=False),
 
 # Botão para deletar arrays
 
@@ -51,7 +50,7 @@ layout = dbc.Col([
         dbc.NavLink('Algoritmos', href='/algorithms', active='exact'),
         html.Hr(),
         html.H2('Algoritmos de Ordenação', className='text-primary'),
-        dbc.NavLink('Bubble Sort', href='/bubble_sort', active='exact'),
+        dbc.NavLink('Bubble Sort', href='/bubble_sort', active='exact', style={"margin-top": "5px"}),
         dbc.NavLink('Gnome Sort', href='/gnome_sort', active='exact'),
         dbc.NavLink('Heap Sort', href='/heap_sort', active='exact'),
         dbc.NavLink('Insertion Sort', href='/insertion_sort', active='exact'),
@@ -65,15 +64,40 @@ layout = dbc.Col([
     # ])
 ], id='sidebar')
 
+sizes: list[int] = [10, 50, 100, 200, 500, 800, 1000, 1500, 2000, 3500, 4500, 5500, 6500, 7500, 8500, 9500, 10000]
 
 
-@app.callback(Output('alert-delete', 'is_open'), Input('delete-arrays', 'n_clicks'), State('delete-arrays', 'is_open'))
-def toggle(n, is_open):
-    if n:
-        return not is_open
-
+# Gera os arrays para os dashboards
 @app.callback(Output('alert-generate', 'is_open'), Input('generate-arrays', 'n_clicks'), State('delete-arrays', 'is_open'))
-def toggle(n, is_open):
-    if n:
+def generate(n, is_open):
+    if n is not None and n > 0:
+        def generate_list(size: list[int]) -> list[int]:
+            list = random.sample(range(0, 10001), size)
+            return list
+        
+        def generate_xls(sizes: list[int]):
+            df = pd.DataFrame(index=range(max(sizes)), columns=sizes)
+            for size in sizes:
+                list = generate_list(size)
+                df[size] = list[:size] + [None] * (max(sizes) - size)
+                print(f'Generated list with {size} elements')
+            try:
+                df.to_excel('app/db/list.xlsx')
+            except:
+                os.mkdir('app/db')
+                df.to_excel('app/db/list.xlsx')
+
+        generate_xls(sizes)
         return not is_open
-    return is_open
+    else:
+        return is_open
+
+# Deleta os arrays
+@app.callback(Output('alert-delete', 'is_open'), Input('delete-arrays', 'n_clicks'), State('delete-arrays', 'is_open'))
+def delete_list(n, is_open):
+    if n is not None and n > 0:
+        try:
+            os.remove('app/db/list.xlsx')
+        except:
+            pass
+        return not is_open
